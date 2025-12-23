fd1978bde190524c61fe3d34d6c892c8ad1189e3
roniellys <roniellys@hotmail.com>
2025-12-23 14:07:12 -0300

    create dto and interfaces services

---
+++ source/Bank.Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+using Bank.Services.DTO;
+
+namespace Bank.Services.interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDTO> Create(UserDTO userDTO);
+        Task<UserDTO> Update(UserDTO userDTO);
+        Task Remove(long id);
+        Task<UserDTO> Get(long id);
+        Task<List<UserDTO>> Get();
+        Task<List<UserDTO>> SearchByName(string name);
+        Task<List<UserDTO>> SearchByEmail(string email);
+        Task<UserDTO> GetByEmail(string email);
+    }    
+}
